435e8ceb3ee9d5b2a898497a1029d27042ab4df8
muhammadhashemi <muhammad.h.0106@gmail.com>
2023-07-30 09:10:22 +0000

    Updated

--- minio/python-minio.py
+++
@@ -1,8 +0,0 @@
-from minio import Minio
-
-client = Minio(endpoint="miinioapi.duckdns.org",
-    access_key="biighunter",
-    secret_key="11236939",
-    secure=True)
-
-print("Total buckets: ", len(client.list_buckets()))
